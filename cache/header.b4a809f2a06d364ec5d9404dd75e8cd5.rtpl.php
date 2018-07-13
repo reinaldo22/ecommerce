6e3f41509fa89e7a17e3da0ed0e1f9d4857c4aa0
reinaldo22 <vargas.reynaldo22@gmail.com>
2018-07-13 15:02:49 -0300

    Criamos a classe Page

---
+++ cache/header.b4a809f2a06d364ec5d9404dd75e8cd5.rtpl.php
@@ -0,0 +1,8 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Hcode store</title>
+    </head>
+    <body>
+        
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once("vendor/autoload.php");
 
@@ -7,13 +7,12 @@ $app = new \Slim\Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	
+    $page = new Hcode\Page();
+    $page->setTpl("index");
 
 });
 
 $app->run();
 
- ?>
+?>
