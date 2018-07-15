9ec41fd18c912e55d9c268070e992f651ce6442d
reinaldo22 <vargas.reynaldo22@gmail.com>
2018-07-14 23:35:03 -0300

    não conseguindo fazer login na pagina login

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php
 
 require_once("vendor/autoload.php");
-
+use Hcode\Model\User;
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
-
-
-$app->get('/admin', function() use ($app) {
-
-    $page = new Hcode\PageAdmin();
-    $page->setTpl("index");
-
-});
-
 
 $app->get('/', function() {
 
@@ -21,6 +12,26 @@ $app->get('/', function() {
     $page->setTpl("index");
 });
 
+$app->get('/admin', function() use ($app) {
+
+    $page = new Hcode\PageAdmin();
+    $page->setTpl("index");
+
+});
+$app->get('/admin/login/', function(){
+
+    $page = new Hcode\PageAdmin(["header"=>false,"footer"=>false]);
+    
+    $page->setTpl("login");
+
+});
+$app->post('/admin/login/', function(){
+
+    User::login($_POST["login"], $_POST["password"]);
+    header("Location: /admin");
+    exit;
+
+});
 
 $app->run();
 
---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hcode\Model;
+
+use Hcode\DB\Sql;
+use \Hcode\Model;
+
+class User extends Model{
+    private $results;
+    public static function login($login, $password){
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+            ":LOGIN"=>$login
+        ));
+        if (count($results)=== 0){
+            throw new \Exception("Usuário inexistente ou senha inválida.");
+        }
+        $data = $results[0];
+        if (password_verify($password,$data["despassword"]) === true){
+
+            $user = new User();
+            $user->setiduser($data["idusers"]);
+        }else{
+            throw new \Exception("Usuário inexistente ou senha inválida.");
+        }
+    }
+}
