793be0a8d98e8d5e63b39e54f3b21ab2c2015b7c
reinaldo22 <vargas.reynaldo22@gmail.com>
2018-07-17 21:41:20 -0300

    nao consigo entrar na pagina usuarios

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once("vendor/autoload.php");
 use Hcode\Model\User;
 $app = new \Slim\Slim();
 
+
 $app->config('debug', true);
 
 $app->get('/', function() {
@@ -42,9 +43,43 @@ $app->get('/admin/logout', function(){
     User::logout();
     header("Location: /admin/login");
     exit;
+});
+$app->get('/admin/users', function(){
+
+    User::verifyLogin();
+    $page = new \Hcode\PageAdmin();
+    $page->setTpl("users");
+});
+
+$app->get('/admin/users/create', function(){
+
+    User::verifyLogin();
+    $page = new \Hcode\PageAdmin();
+    $page->setTpl("users-create");
+});
+$app->get('/admin/users/:iduser', function($iduser){
+
+    User::verifyLogin();
+    $page = new \Hcode\PageAdmin();
+    $page->setTpl("users-update");
+});
+$app->post('/admin/users/create', function(){
+
+    User::verifyLogin();
+
+});
+$app->post('/admin/users/:iduser', function($iduser){
+
+    User::verifyLogin();
+
+});
+$app->delete('/admin/users/:iduser', function($iduser){
+
+    User::verifyLogin();
 
 });
 
 $app->run();
 
 ?>
+
